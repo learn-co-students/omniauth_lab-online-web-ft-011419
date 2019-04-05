ae5680bfeba11f931ada3729408f975fd0b67fda
gbs4ever <gbs4ever2@gmail.com>
2019-04-05 15:46:29 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+ # resources :users
+ #get '/welcome' => 'welcome#home'
+  get '/auth/facebook/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
